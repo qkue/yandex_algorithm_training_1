95df734591c3127d50a51530aa60316bf037afe0
qkue <130838079+qkue@users.noreply.github.com>
2024-04-26 14:22:25 +0300

    Update solution.py

--- Exercise 5/I/solution.py
+++ Exercise 5/I/solution.py
@@ -1,2 +1,16 @@
 # Python 3.12.1
 
+n = int(input())
+arr = input()
+
+result = 0
+cum_pref = 0
+
+for letter in range(n, len(arr)):
+    if arr[letter] == arr[letter - n]:
+        cum_pref += 1
+        result += cum_pref
+    else:
+        cum_pref = 0
+
+print(result)
