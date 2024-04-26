9b69ca9eba50eeb689761a478eb1a57d4b6be4bb
qkue <130838079+qkue@users.noreply.github.com>
2024-04-26 14:35:27 +0300

    Update solution.py

--- Exercise 6/C/solution.py
+++ Exercise 6/C/solution.py
@@ -1,2 +1,15 @@
 # Python 3.12.1
 
+w, h, n = map(int, input().split())
+
+left = 0
+right = 10 ** 14
+
+while left < right:
+    m = (left + right) // 2
+    if ((m // w) * (m // h)) >= n:
+        right = m
+    else:
+        left = m + 1
+
+print(left)
