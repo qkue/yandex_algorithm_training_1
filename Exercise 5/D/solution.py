eeec159dd318e2908192c4b1c70d40839b3166e0
qkue <130838079+qkue@users.noreply.github.com>
2024-04-26 14:17:06 +0300

    Update solution.py

--- Exercise 5/D/solution.py
+++ Exercise 5/D/solution.py
@@ -1,2 +1,13 @@
 # Python 3.12.1
 
+n, r = map(int, input().split())
+
+arr = list(map(int, input().split()))
+
+pair = 0
+last = 0
+for first in range(n):
+    while last < n and arr[last] - arr[first] <= r:
+        last += 1
+    pair += n - last
+print(pair)
