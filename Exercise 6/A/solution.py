a4b7e328ad691317869f6ff41cfd8c8a99ab3a08
qkue <130838079+qkue@users.noreply.github.com>
2024-04-26 14:29:49 +0300

    Update solution.py

--- Exercise 6/A/solution.py
+++ Exercise 6/A/solution.py
@@ -1,2 +1,19 @@
 # Python 3.12.1
 
+n, k = map(int, input().split())
+arr_n = list(map(int, input().split()))
+arr_k = list(map(int, input().split()))
+
+for target in arr_k:
+    left = 0
+    right = n - 1
+    while left < right:
+        m = (left + right) // 2
+        if arr_n[m] >= target:
+            right = m
+        else:
+            left = m + 1
+    if arr_n[left] != target:
+        print('NO')
+    else:
+        print('YES')
