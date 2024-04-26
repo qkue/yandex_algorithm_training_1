44679f4ecad4a8dc493b5572bbaa714f7c36ed99
qkue <130838079+qkue@users.noreply.github.com>
2024-04-26 13:22:41 +0300

    Update solution.py

--- Exercise 3/G/solution.py
+++ Exercise 3/G/solution.py
@@ -1,2 +1,15 @@
 # Python 3.12.1
 
+n = int(input())
+result = 0
+turtle = set()
+#temp = 0
+
+for _ in range(n):
+#    temp = 0
+    forward, back = map(int, input().split())
+    if abs(back) + 1 + abs(forward) == n and (forward, back) not in turtle:
+        turtle.add((forward, back))
+        result += 1
+
+print(result)
