8f6ba72b12bbe2c6d9cccca07589942f614500b2
qkue <130838079+qkue@users.noreply.github.com>
2024-04-26 13:32:36 +0300

    Update solution.py

--- Exercise 4/D/solution.py
+++ Exercise 4/D/solution.py
@@ -1,2 +1,18 @@
 # Python 3.12.1
 
+n = int(input())
+
+keyboard = dict()
+keys_push = list(map(int, input().split()))
+
+for i in range(1, n+1):
+    keyboard[i] = keys_push[i-1]
+
+push = int(input())
+key_seq = list(map(int, input().split()))
+
+for key in key_seq:
+    keyboard[key] -= 1
+
+for key in keyboard:
+    print('YES' if keyboard[key] < 0 else 'NO')
