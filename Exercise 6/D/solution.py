a498976a55646989f9c4f2a415e8ed908447c91b
qkue <130838079+qkue@users.noreply.github.com>
2024-04-26 14:39:24 +0300

    Update solution.py

--- Exercise 6/D/solution.py
+++ Exercise 6/D/solution.py
@@ -1,2 +1,15 @@
 # Python 3.12.1
 
+n, a, b, w, h = map(int, input().split())
+
+left = 0
+right = 10 ** 18
+while left < right:
+    m = (left + right + 1) // 2
+    #print(f'm = {m} w * h - {((w // (a + 2 * m)) * (h // (b + 2 * m)))} or {((w // (b + 2 * m)) * (h // (a + 2 * m)))}')
+    if (((w // (a + 2 * m)) * (h // (b + 2 * m))) >= n or ((w // (b + 2 * m)) * (h // (a + 2 * m))) >= n):
+        left = m
+    else:
+        right = m - 1
+
+print(left)
