14b3c7d151a29f2a4ac288dea425db72898cde5f
qkue <130838079+qkue@users.noreply.github.com>
2024-04-26 15:05:06 +0300

    Update solution.py

--- Exercise 7/C/solution.py
+++ Exercise 7/C/solution.py
@@ -1,2 +1,32 @@
 # Python 3.12.1
 
+from collections import deque
+
+n, d = map(int, input().split())
+students = ((table, i) for i, table in enumerate(map(int, input().split())))
+events = []
+for student in students:
+    table, num = student
+    events.append((table, 0, num))
+    events.append((table + d, 1, num))
+events.sort()
+
+n_tickets = 0
+free_tickets = deque()
+assignment = [None] * n
+for table, operation, ind in events:
+    if operation:
+        free_tickets.append(assignment[ind][0])
+    else:
+        if free_tickets:
+            t = free_tickets.popleft()
+        else:
+            n_tickets += 1
+            t = n_tickets
+        assignment[ind] = (t, ind)
+assignment.sort(key = lambda p: p[1])
+print(n_tickets)
+ans = []
+for ticket in assignment:
+    ans.append(str(ticket[0]))
+print(' '.join(ans))
