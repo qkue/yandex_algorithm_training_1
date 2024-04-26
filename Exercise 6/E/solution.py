2875e51e84fde0157ec44632dead61302b418a36
qkue <130838079+qkue@users.noreply.github.com>
2024-04-26 14:42:01 +0300

    Update solution.py

--- Exercise 6/E/solution.py
+++ Exercise 6/E/solution.py
@@ -1,2 +1,16 @@
 # Python 3.12.1
 
+a = int(input())
+b = int(input())
+c = int(input())
+
+left = 0
+right = 10 ** 18
+grade_cnt = sum([a, b, c])
+while left < right:
+    m = (left + right) // 2
+    if (35 * (grade_cnt + m)) <= (2 * a + 3 * b + 4 * c + 5 * m) * 10:
+        right = m
+    else:
+        left = m + 1
+print(left)
