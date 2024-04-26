eb9dc0ac84688b18e389db87d9fe90a5b195afbb
qkue <130838079+qkue@users.noreply.github.com>
2024-04-26 12:04:05 +0300

    Update solution.py

--- Exercise 2/E/solution.py
+++ Exercise 2/E/solution.py
@@ -1,2 +1,18 @@
 # Python 3.12.1
 
+n = int(input())
+arr = list(map(int, input().split()))
+
+winner = arr.index(max(arr))
+result = 0
+
+for num in range(1, len(arr) - 1):
+        if num > winner and ((arr[num] + 5) % 10) == 0 and arr[num+1] < arr[num] and arr[num] > result:
+
+            result = arr[num]
+
+if result:
+    arr.sort(reverse=True)
+    result = arr.index(result) + 1
+
+print(result)
