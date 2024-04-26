cfd684085a8500c813c4f731a0999a9da4fc3227
qkue <130838079+qkue@users.noreply.github.com>
2024-04-26 13:23:44 +0300

    Update solution.py

--- Exercise 3/H/solution.py
+++ Exercise 3/H/solution.py
@@ -1,2 +1,15 @@
 # Python 3.12.1
 
+n = int(input())
+result = 0
+
+birds = [tuple(map(int, input().split())) for _ in range(n)]
+birds.sort(key = lambda x: (x[0], -x[1]))
+
+shoot = set()
+for x, y in birds:
+    if x not in shoot:
+        shoot.add(x)
+        result += 1
+
+print(result)
