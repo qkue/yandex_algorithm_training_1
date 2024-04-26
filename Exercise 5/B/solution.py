200a9750711ff81b45754996c62f0291a3dae567
qkue <130838079+qkue@users.noreply.github.com>
2024-04-26 14:15:04 +0300

    Update solution.py

--- Exercise 5/B/solution.py
+++ Exercise 5/B/solution.py
@@ -1,2 +1,20 @@
 # Python 3.12.1
 
+n, k = map(int, input().split())
+
+arr = list(map(int, input().split()))
+
+result = 0
+delta = 0
+last = 0
+
+for first in range(n):
+	while last < n and (last == first or delta + arr[last] <= k):
+		if delta + arr[last] == k:
+			result += 1
+			break
+		delta += arr[last]
+		last += 1
+	delta -= arr[first]
+        
+print(result)
