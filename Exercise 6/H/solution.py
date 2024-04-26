4fb4cf38d37aa9a5f5cd6c1daadd5818cb869f3b
qkue <130838079+qkue@users.noreply.github.com>
2024-04-26 14:53:51 +0300

    Update solution.py

--- Exercise 6/H/solution.py
+++ Exercise 6/H/solution.py
@@ -1,2 +1,23 @@
 # Python 3.12.1
 
+n, k = map(int, input().split())
+arr = [int(input()) for _ in range(n)]
+
+left = 0
+right = 10 ** 7
+
+def check(k, m, arr):
+    if k <= sum([line // m for line in arr]):
+        return True
+    else:
+        return False
+
+while left < right:
+    mid = (left + right + 1) // 2
+    
+    if check(k, mid, arr):
+        left = mid
+    else:
+        right = mid - 1
+
+print(left)
