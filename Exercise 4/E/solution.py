8ce28261aea53f20f71c8d7599809a324240463b
qkue <130838079+qkue@users.noreply.github.com>
2024-04-26 13:34:30 +0300

    Update solution.py

--- Exercise 4/E/solution.py
+++ Exercise 4/E/solution.py
@@ -1,2 +1,17 @@
 # Python 3.12.1
 
+n = int(input())
+blocks = {}
+
+for _ in range(n):
+    w, h = map(int, input().split())
+    if w not in blocks:
+        blocks[w] = 0
+    if blocks[w] < h:
+        blocks[w] = h
+
+blocks_sort_by_keys = sorted(blocks, reverse=True)
+result = 0
+for block in blocks_sort_by_keys:
+    result += blocks[block]
+print(result)
