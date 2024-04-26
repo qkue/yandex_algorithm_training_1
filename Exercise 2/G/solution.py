717e4a1fdab1f74851a13cfe259c1f917786419e
qkue <130838079+qkue@users.noreply.github.com>
2024-04-26 13:12:05 +0300

    Update solution.py

--- Exercise 2/G/solution.py
+++ Exercise 2/G/solution.py
@@ -1,2 +1,15 @@
 # Python 3.12.1
 
+arr = list(map(int, input().split()))
+
+arr.sort()
+
+x1 = arr[0]
+x2 = arr[1]
+y1 = arr[-1]
+y2 = arr[-2]
+
+if x1 * x2 > y1 * y2:
+	print(x1, x2)
+else:
+	print(y2, y1)
