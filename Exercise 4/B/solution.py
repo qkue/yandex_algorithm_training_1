f6354e51836e420791eb0bfe0ac2345861150f7a
qkue <130838079+qkue@users.noreply.github.com>
2024-04-26 13:28:20 +0300

    Update solution.py

--- Exercise 4/B/solution.py
+++ Exercise 4/B/solution.py
@@ -1,2 +1,12 @@
 # Python 3.12.1
 
+with open('input.txt', 'rt', encoding='utf-8') as file:
+    text = file.read().split()
+
+word_dict = {}
+
+for word in text:
+    if word not in word_dict:
+        word_dict[word] = 0
+    print(word_dict[word], end=' ')
+    word_dict[word] += 1
