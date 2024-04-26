3412de22be200a8973af364451fcc3cb7940a042
qkue <130838079+qkue@users.noreply.github.com>
2024-04-26 13:27:28 +0300

    Update solution.py

--- Exercise 4/A/solution.py
+++ Exercise 4/A/solution.py
@@ -1,2 +1,11 @@
 # Python 3.12.1
 
+n = int(input())
+result = {}
+
+for _ in range(n):
+    left, right = input().split()
+    result[left] = right
+    result[right] = left
+
+print(result[input()])
