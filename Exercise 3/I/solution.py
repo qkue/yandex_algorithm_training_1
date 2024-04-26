c96cd57da6df540f15f1a06d93fd9063b40c6a19
qkue <130838079+qkue@users.noreply.github.com>
2024-04-26 13:24:40 +0300

    Update solution.py

--- Exercise 3/I/solution.py
+++ Exercise 3/I/solution.py
@@ -1,2 +1,19 @@
 # Python 3.12.1
 
+n = int(input())
+all_set = {input() for _ in range(1) for _ in range(int(input()))}
+all_know = all_set.copy()
+
+for _ in range(n-1):
+    temp = set()
+    for _ in range(int(input())):
+        lang = input()
+        temp.add(lang)
+        all_set.add(lang)
+    all_know &= temp
+
+print(len(all_know))
+if all_know:
+    print(*all_know, sep = '\n')
+print(len(all_set))
+print(*all_set, sep = '\n')
