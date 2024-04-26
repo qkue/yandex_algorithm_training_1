b79a09571e4d5d7da2a901e457e9d9539be798af
qkue <130838079+qkue@users.noreply.github.com>
2024-04-26 11:55:47 +0300

    Update solution.py

--- Exercise 2/A/solution.py
+++ Exercise 2/A/solution.py
@@ -1,2 +1,19 @@
 # Python 3.12.1
 
+arr = list(map(int, input().split()))
+
+last_seen = 0
+flag_seq = 0
+
+if len(arr) > 0:
+    last_seen = arr[0]
+for i in arr[1:]:
+    if last_seen >= i:
+        flag_seq = 1
+        break
+    last_seen = i
+
+if flag_seq:
+    print('NO')
+else:
+    print('YES')
