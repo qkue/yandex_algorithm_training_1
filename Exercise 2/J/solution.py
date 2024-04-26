cef5a34569cf86f9dd8ff26196029018fe02ac76
qkue <130838079+qkue@users.noreply.github.com>
2024-04-26 13:15:11 +0300

    Update solution.py

--- Exercise 2/J/solution.py
+++ Exercise 2/J/solution.py
@@ -1,2 +1,23 @@
 # Python 3.12.1
 
+n = int(input())
+prev = float(input())
+left = 30.0
+right = 4000.0
+
+for row in range(n-1):
+    new, text = input().strip().split()
+    new = float(new)
+    if text == 'closer':
+        if new < prev:
+            right = min(right, ((new + prev) / 2))
+        else:
+            left = max(left, ((new + prev) / 2))
+    else:
+        if new > prev:
+            right = min(right, ((new + prev) / 2))
+        else:
+            left = max(left, ((new + prev) / 2))
+    prev = new
+
+print(left, right)
