e6202800dd58dc638f3882e71e17cb9e831f88db
qkue <130838079+qkue@users.noreply.github.com>
2024-04-26 13:21:39 +0300

    Update solution.py

--- Exercise 3/F/solution.py
+++ Exercise 3/F/solution.py
@@ -1,2 +1,14 @@
 # Python 3.12.1
 
+first = input()
+second = input()
+second_set = {second[ind] + second[ind+1] for ind in range(len(second) - 1)}
+result = 0
+
+for ind in range(len(first) - 1):
+    pair = first[ind] + first[ind+1]
+    if pair in second_set:
+        
+        result += 1
+
+print(result)
