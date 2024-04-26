55a0f1d58125734d7a57140f7b96dfd5b570b39d
qkue <130838079+qkue@users.noreply.github.com>
2024-04-26 13:18:41 +0300

    Update solution.py

--- Exercise 3/C/solution.py
+++ Exercise 3/C/solution.py
@@ -1,2 +1,20 @@
 # Python 3.12.1
 
+n, m = map(int, input().split())
+anna_cubs = {int(input()) for _ in range(n)}
+borya_cubs = {int(input()) for _ in range(m)}
+
+intersection_cubs = sorted(anna_cubs & borya_cubs)
+print(len(intersection_cubs))
+if intersection_cubs:
+    print(*intersection_cubs)
+
+annas_dif = sorted(anna_cubs - borya_cubs)
+print(len(annas_dif))
+if annas_dif:
+    print(*annas_dif)
+
+borya_dif = sorted(borya_cubs - anna_cubs)
+print(len(borya_dif))
+if borya_dif:
+    print(*borya_dif)
