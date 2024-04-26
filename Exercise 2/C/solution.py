4bcb68afcc36a31a130a4b0ecaaa22d95c930d73
qkue <130838079+qkue@users.noreply.github.com>
2024-04-26 11:58:40 +0300

    Update solution.py

--- Exercise 2/C/solution.py
+++ Exercise 2/C/solution.py
@@ -1,2 +1,15 @@
 # Python 3.12.1
 
+n = int(input())
+arr = list(map(int, input().split()))
+x = int(input())
+
+ans = arr[0]
+delta = abs(x - ans)
+
+for num in arr:
+    if abs(x - num) < delta:
+        delta = abs(x - num)
+        ans = num
+
+print(ans)
