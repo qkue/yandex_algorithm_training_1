e9a94284f21d7da4c0f27ff4b70e41cba9cdab78
qkue <130838079+qkue@users.noreply.github.com>
2024-04-26 14:44:44 +0300

    Update solution.py

--- Exercise 6/F/solution.py
+++ Exercise 6/F/solution.py
@@ -1,2 +1,19 @@
 # Python 3.12.1
+# Задача действительно легкая, без каких-либо камней :)
 
+n, x, y = map(int, input().split())
+
+if x > y:
+    x, y = y, x
+
+left = 0
+right = n * y
+
+while left < right:
+    m = (left + right) // 2
+    if m // x + m // y >= n - 1:
+        right = m
+    else:
+        left = m + 1
+
+print(left + x)
