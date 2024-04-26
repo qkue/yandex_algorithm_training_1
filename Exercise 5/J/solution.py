1cde8580e3f6f98a3b81b1a711df928c93f9b3fe
qkue <130838079+qkue@users.noreply.github.com>
2024-04-26 14:24:28 +0300

    Update solution.py

--- Exercise 5/J/solution.py
+++ Exercise 5/J/solution.py
@@ -1,2 +1,28 @@
-# Python 3.12.1
+# Python 3.9 (PyPy 7.3.11)
 
+n = int(input())
+points = [tuple(map(int, input().split())) for _ in range(n)]
+result = 0
+
+for ind in range(n):
+    used_points = set()
+    neig = []
+
+    for j in range(n):
+        x = points[ind][0] - points[j][0]
+        y = points[ind][1] - points[j][1]
+        length = x ** 2 + y ** 2
+        neig.append(length)
+        if (x, y) in used_points:
+            result -= 1
+        used_points.add((-x, -y))
+
+    neig.sort()
+    
+    last = 0
+    for first in range(len(neig)):
+        while last < len(neig) and neig[last] == neig[first]:
+            last += 1
+        result += last - first - 1
+
+print(result)
