bc15bf76a9fb757f51b789fb02bfb7f79884c64c
qkue <130838079+qkue@users.noreply.github.com>
2024-04-26 14:55:00 +0300

    Update solution.py

--- Exercise 6/I/solution.py
+++ Exercise 6/I/solution.py
@@ -1,2 +1,28 @@
 # Python 3.12.1
 
+n, r, c = map(int, input().split())
+height = [int(input()) for _ in range(n)]
+height.sort()
+
+left = 0
+right = height[-1]
+
+def check(m, arr, n, c):
+    count = 0
+    teams = 0
+    while count < n - c + 1:
+        if arr[count + c - 1] - arr[count] <= m:
+            teams += 1
+            count += c
+        else:
+            count += 1
+    return teams
+
+while left < right:
+    mid = (left + right) // 2
+    if check(mid, height, n, c) >= r:
+        right = mid
+    else:
+        left = mid + 1
+
+print(left)
