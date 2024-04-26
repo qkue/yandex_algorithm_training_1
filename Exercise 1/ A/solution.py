ca63d85a55f64ce01f2062f2308cae9cd333a0f9
qkue <130838079+qkue@users.noreply.github.com>
2024-04-26 09:48:53 +0300

    Create solution.py

---
+++ Exercise 1/ A/solution.py
@@ -0,0 +1,14 @@
+def conditioner(room, cond, mode):
+    if mode == 'freeze':
+        print(min(cond, room))
+    elif mode == 'heat':
+        print(max(cond, room))
+    elif mode == 'auto':
+        print(cond)
+    else:
+        print(room)
+
+
+troom, tcond = map(int, input().split())
+cond_mode = input()
+conditioner(troom, tcond, cond_mode)
